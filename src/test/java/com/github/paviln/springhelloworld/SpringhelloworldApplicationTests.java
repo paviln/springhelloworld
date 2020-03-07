424cf1940e1f66509414013e75e2eede490de57e
Jens Christensen <paviln@outlook.dk>
2020-03-07 18:14:30 +0100

    Generate new Spring project

---
+++ src/test/java/com/github/paviln/springhelloworld/SpringhelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paviln.springhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringhelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
